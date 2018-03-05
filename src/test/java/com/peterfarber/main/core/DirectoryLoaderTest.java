2463f5167df1a4cfe64176c8249bf1b6c9e746e3
Peter Farber <peterfarber@fullsail.edu>
2018-03-05 14:12:52 -0500

    Finished test cases for DirectoryLoader.

--- src/test/java/com/peterfarber/main/core/DirectoryLoaderTest.java
+++ src/test/java/com/peterfarber/main/core/DirectoryLoaderTest.java
@@ -4,38 +4,98 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 public class DirectoryLoaderTest {
 
+    DirectoryLoader<User> userLoader;
+    ArrayList<User> users;
+
+
     @Before
     public void setUp() throws Exception {
-
-        
+        users = new ArrayList<User>();
+        for(int i = 0; i < 10; i++){
+            User user = new User(i+"000", i+"0000", "password");
+            user.save();
+            System.out.println(user.getUsername());
+            users.add(user);
+        }
+        TimeUnit.SECONDS.sleep(1);
 
     }
 
     @After
     public void tearDown() throws Exception {
+        for(int i = 0; i < users.size(); i++){
+            users.get(i).delete();
+        }
     }
 
     @Test
     public void load() {
+        try {
+            userLoader = new DirectoryLoader<User>("data/users");
+            userLoader.load();
+        }catch (Exception e){
+
+        }finally {
+            assertTrue(userLoader.getSize() > 0);
+        }
     }
 
     @Test
     public void getSize() {
+        try {
+            userLoader = new DirectoryLoader<User>("data/users");
+            userLoader.load();
+        }catch (Exception e){
+
+        }finally {
+            assertTrue(userLoader.getSize() == 10);
+        }
     }
 
     @Test
     public void getIndex() {
+        try {
+            userLoader = new DirectoryLoader<User>("data/users");
+            userLoader.load();
+        }catch (Exception e){
+
+        }finally {
+            assertTrue(userLoader.getIndex(2).getUsername().equals("20000"));
+        }
     }
 
     @Test
     public void add() {
+        try {
+            userLoader = new DirectoryLoader<User>("data/users");
+            userLoader.load();
+            User userAdd = new User("Test", "Case", "Pass");
+            userLoader.add(userAdd);
+        }catch (Exception e){
+
+        }finally {
+            assertTrue(userLoader.getIndex(10).getUsername().equals("Case"));
+            assertTrue(userLoader.getSize() == 11);
+        }
     }
 
     @Test
     public void remove() {
+        try {
+            userLoader = new DirectoryLoader<User>("data/users");
+            userLoader.load();
+            userLoader.remove(userLoader.getIndex(0));
+        }catch (Exception e){
+
+        }finally {
+            assertTrue(userLoader.getSize() == 9);
+        }
     }
 }
